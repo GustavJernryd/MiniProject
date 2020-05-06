29d4b8ddcb30ac9f16d8e8b4c6aa8f9e9c410a88
adamOvi <adam.alfo96@hotmail.com>
2020-05-06 13:19:59 +0200

    Bullet
    bullet_prefab

---
+++ WildWest/Assets/Scripts/bullet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    [SerializeField] private float sf_Bullet_Speed;
+    [SerializeField] private GameObject bullet_prefab;
+    [SerializeField] private GameObject sf_Player1_Prefab;
+    [SerializeField] private GameObject sf_Player2_Prefab;
+
+    float speed;
+    private GameObject player1;
+    private GameObject player2;
+
+    private GameObject bullet_Object;
+
+    private Vector3 direction;
+    private Vector3 bullet_Position;
+
+    public List<GameObject> bulletList;
+
+    Vector3 spawnBulletPos;
+
+    //public bullet(Vector3 spawnPos, Vector3 dir)
+    //{
+    //    bullet_Object = Instantiate(bullet_prefab);
+    //    this.direction = dir;
+    //    this.spawnBulletPos = spawnPos;
+    //    bullet_Object.transform.position = spawnBulletPos;
+    //    transform.Rotate(new Vector3(0 , 90, 0)); // fungerar ej...
+    //}
+
+    public void Awake()
+    {
+        speed = sf_Bullet_Speed;
+        //player1 = sf_Player1_Prefab;
+        //player2 = sf_Player2_Prefab;
+
+        //spawnbullet_Gameobject = GameObject.FindGameObjectWithTag("Bullet_SpawnPos_Player1");
+    }
+
+    public void Update()
+    {
+        transform.position +=  new Vector3(0,0,-1) * speed * Time.deltaTime;
+
+    }
+
+
+}
