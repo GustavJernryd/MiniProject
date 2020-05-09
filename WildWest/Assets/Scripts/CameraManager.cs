a7055e24b75c94e0f01126bc9918319a006f22a0
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2020-05-09 16:24:20 +0200

    lade till cameraanimeringar och cameraPrefab som kontrollerar cameran med animeringarna

---
+++ WildWest/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    private Animator CameraAnimator;
+    [SerializeField] private GameObject PlayerCamera;
+    [SerializeField] private GameObject CinematicCamera;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            StartRotateAnimation();
+        }
+    }
+
+    public void StartRotateAnimation()
+    {
+        PlayerCamera.SetActive(false);
+        CinematicCamera.SetActive(true);
+        CameraAnimator.Play("Camera_Rotation_Animation",0,0f);
+    }
+
+    public void EndAnimationSequence()
+    {  
+        PlayerCamera.SetActive(true);
+        CinematicCamera.SetActive(false);
+    }
+}
