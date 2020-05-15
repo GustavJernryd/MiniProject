2cf7a5723ceba1369017fab83f826470ac760d91
GustavJernryd <31235205+GustavJernryd@users.noreply.github.com>
2020-05-15 13:23:14 +0200

    Revert "Merge branch 'Sikte'"
    This reverts commit 6440a50266c931a565d96cac10151597eb2970de, reversing
    changes made to 7dc0889d3c6c3bc587b2829f66b3cb24b3d17771.

--- WildWest/Assets/Scripts/PlayerMovement.cs
+++ WildWest/Assets/Scripts/PlayerMovement.cs
@@ -8,31 +8,34 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed = 4;
     [SerializeField] private bool movableY;
 
-    public float speedH = 2.0f;
-    public float speedV = 2.0f;
-
-    private float yaw = 180.0f;
-    private float pitch = 0.0f;
-
-    Vector3 movement;
+    Vector3 movement, startPosition;
     Vector2 direction;
 
+    void Start()
+    {
+        startPosition = transform.position;
+    }
     // Update is called once per frame
     void Update()
     {
-        direction.x = Input.GetAxis("Horizontal");
-
-        if (movableY)
+        if(GameManager.instance.gameState == GameState.Menu && GameManager.instance.oldGameState == GameState.Running)
         {
-        direction.y = Input.GetAxis("Vertical");
-        }   
+            transform.position = startPosition;
+        }
 
-        movement = new Vector3(direction.x, 0, direction.y);
-        charCont.Move(movement * speed * Time.deltaTime);
 
-        yaw += speedH * Input.GetAxis("Mouse X");
-        pitch += speedV * Input.GetAxis("Mouse Y");
-        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        if(GameManager.instance.gameState == GameState.Running)
+        {
+            direction.x = Input.GetAxis("Horizontal");
+
+            if (movableY)
+            {
+            direction.y = Input.GetAxis("Vertical");
+            }   
+
+            movement = new Vector3(direction.x, 0, direction.y);
+            charCont.Move(movement * speed * Time.deltaTime);
+        }
     }
 
     
