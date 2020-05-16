0192d08084a0f90f21d93d401a9c07cd9fec751a
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2020-05-16 19:01:41 +0200

    Skapade en thumbleweed

---
+++ WildWest/Assets/WindForce.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindForce : MonoBehaviour
+{
+    [SerializeField] float force;
+    [SerializeField] float coolDown;
+
+    private float currentTime;
+    private Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = coolDown;
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+
+        if(currentTime <= 0)
+        {
+            currentTime = coolDown;
+            ApplyForce();
+            Debug.Log("Blow");
+        }
+    }
+
+    void ApplyForce()
+    {
+        Vector3 dir;
+        dir = new Vector3(Random.Range(-1f, 1f),0f, Random.Range(-1f, 1f));
+        rigid.AddForce(dir * force);
+
+
+    }
+}
